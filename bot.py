08d37fa9186c4684ae9a0a6b6e0051f3459877d0
Nitrix <120071588+nitrixdev@users.noreply.github.com>
2022-12-10 20:07:41 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import os
+import random
+import string
+import colorama
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+import threading, requests, ctypes, os, time, random, string
+from datetime import datetime
+from colorama import Fore, Style
+from random import *
+
+PATH = "chromedriver.exe"
+
+amount = 0
+error = 0
+
+os.system(f"title Youtube View Bot ^| Views: {amount} ^| Errors: {error}")
+
+os.system("cls")
+
+url = input("> Enter Video URL: ")
+count = input("> Enter View Count: ")
+
+delay = input("> Enter Watch Time (seconds): ")
+
+
+while count == count:
+    driver = webdriver.Chrome(PATH)
+    driver.minimize_window()
+    os.system(f"title Youtube View Bot ^| Views: {amount} ^| Errors: {error}")
+    driver.get(url)
+    time.sleep(delay)
+    amount += 1
