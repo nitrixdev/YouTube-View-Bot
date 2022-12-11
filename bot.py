cb7a9a2ba3632240f4174418dbf62c8a22650db5
Nitrix <120071588+nitrixdev@users.noreply.github.com>
2022-12-11 17:30:28 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,7 @@
-import undetected_chromedriver as uc
+from selenium import webdriver
 import time, os
 from colorama import Fore, Back, Style
 
-os.system("title View Bot V1 ^| Success: 0")
-
-url = input("> Enter Video Link: ")
-views = input("> Enter Amount Of Views: ")
-delay = int(input("> Enter Watchtime: "))
-
-count = 0
-
-uc.install(
-    executable_path='chromedriver.exe',
-)
 
 print(f"""{Fore.RED}
         _               _           _   
@@ -22,13 +11,21 @@ print(f"""{Fore.RED}
    \_/ |_|\___| \_/\_/ |_.__/ \___/ \__|
                                         """)
 
+os.system("title View Bot V1 ^| Success: 0")
+
+url = input("> Enter Video Link: ")
+views = input("> Enter Amount Of Views: ")
+delay = int(input("> Enter Watchtime: "))
+
+count = 0
+
 opts = uc.ChromeOptions()
 driver = uc.Chrome(options=opts)
 for i in range(views): 
     os.system(f"title View Bot V1 ^| Success: {str(count)}")
-    driver.get(url)
+    siddharth3.get("https://youtu.be/OU69JaZ1FQo")
     time.sleep(delay)
-    driver.refresh()
+    siddharth3.refresh()
     print(f"{Fore.GREEN}Sent view!")
     count += 1
     
